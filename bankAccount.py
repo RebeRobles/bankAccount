314db924a535731fa74dbc7460df6665ea285c01
RebeRobles <rebeca.robles@gmail.com>
2021-07-20 14:02:57 -0400

    Actividad de movimientos de cuenta bancaria

---
+++ bankAccount.py
@@ -0,0 +1,32 @@
+class BankAccount:
+
+	def __init__(self, int_rate = 0.01, balance = 0):
+            self.int_rate = int_rate
+            self.balance = balance
+
+	def deposit(self, amount):
+            self.balance += amount
+            return self
+		
+	def withdraw(self, amount):
+            if self.balance >= amount:
+                self.balance -= amount
+            else:
+                print('Fondos insuficientes: cobrar una tarifa de $5')
+                self.balance -= 5
+            return self
+
+	def display_account_info(self):
+            print(f'Saldo de: $ {self.balance}')
+
+	def yield_interest(self):
+            if self.balance > 0:
+                self.balance += (self.balance * self.int_rate)
+            return self
+
+
+account1 = BankAccount(0.003, 1000)
+account1.deposit(1000).deposit(200).deposit(500).withdraw(800).yield_interest().display_account_info()
+
+account2 = BankAccount()
+account2.deposit(1000).deposit(2000).withdraw(500).withdraw(500).withdraw(1000).withdraw(1000).withdraw(1000).yield_interest().display_account_info()
